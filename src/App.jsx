59d988a85aac38c91642688e0bd8703bbc5d9ae3
Papa G <florentgerard@sfr.fr>
2025-06-12 19:04:40 +0200

    changement de l'ordre d'apparition des sections et renommage blog pour actuality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import About from "./pages/About";
 import Home from "./pages/Home";
-import Pages from "./pages/Blog";
+import Actuality from "./pages/Actuality";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Header /> {/* Affichage du Header */}
       <About /> {/* Affichage de la page "À propos" */}
       <Home /> {/* Affichage de la page d'accueil */}
-      <Pages /> {/* Affichage des autres pages du site */}
+      <Actuality /> {/* Affichage des actualités de l'association */}
       <Footer /> {/* Affichage du Footer */}
     </div>
   );

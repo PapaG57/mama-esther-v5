82afdcbff509b7b69f1880c8ea8f8e154a6eb8b8
Papa G <florentgerard@sfr.fr>
2025-06-12 18:29:40 +0200

    création de la page "about" et du fichier about.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
 import About from "./pages/About";
+import Home from "./pages/Home";
 import Pages from "./pages/Blog";
 
 function App() {
   return (
     <div>
       <Header /> {/* Affichage du Header */}
-      <Home /> {/* Affichage de la page d'accueil */}
       <About /> {/* Affichage de la page "À propos" */}
+      <Home /> {/* Affichage de la page d'accueil */}
       <Pages /> {/* Affichage des autres pages du site */}
       <Footer /> {/* Affichage du Footer */}
     </div>

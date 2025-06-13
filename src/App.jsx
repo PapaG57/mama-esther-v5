53f8d4e5c614088064202145bc49d97adb8b4918
Papa G <florentgerard@sfr.fr>
2025-06-13 19:33:05 +0200

    changement de nom : home pour engagement

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import About from "./pages/About";
-import Home from "./pages/Home";
+import Engagement from "./pages/Engagement";
 import Actuality from "./pages/Actuality";
 import Footer from "./components/Footer";
 
@@ -10,7 +10,7 @@ function App() {
     <div>
       <Header /> {/* Affichage du Header */}
       <About /> {/* Affichage de la page "À propos" */}
-      <Home /> {/* Affichage de la page d'accueil */}
+      <Engagement /> {/* Affichage de la page des engagements */}
       <Actuality /> {/* Affichage des actualités de l'association */}
       <Footer /> {/* Affichage du Footer */}
     </div>
--- src/pages/Engagement.jsx
+++ src/pages/Engagement.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/home.css";
+import "../styles/engagement.css";
 import Divider from "../components/Divider";
 
 function Home() {

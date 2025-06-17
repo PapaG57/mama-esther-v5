79e74a5ed1dffcf5e6aa91e992d40d49016afd86
Papa G <florentgerard@sfr.fr>
2025-06-17 11:53:04 +0200

    reconfiguration du code suite à l'ajout de la page Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
-import About from "./pages/About";
 import Engagement from "./pages/Engagement";
 import Actuality from "./pages/Actuality";
+import About from "./pages/About";
 import Footer from "./components/Footer";
 
-function App() {
+export default function App() {
   return (
-    <>
+    <Router>
       <div className="main-wrapper">
-        <Header />
-        <About />
-        <Engagement />
-        <Actuality />
+        <Routes>
+          {/* Page d’accueil : exactement tes trois sections existantes */}
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Engagement />
+                <Actuality />
+              </>
+            }
+          />
+
+          {/* Nouvelle page À propos (carrousel + texte) */}
+          <Route path="/about" element={<About />} />
+        </Routes>
+
+        {/* Pied de page commun */}
+        <Footer />
       </div>
-      <Footer />
-    </>
+    </Router>
   );
 }
-
-export default App;

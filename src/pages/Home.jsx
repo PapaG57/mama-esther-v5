b3ec928b115709204fe86508632bc620810e2aa2
Papa G <florentgerard@sfr.fr>
2025-06-17 11:50:59 +0200

    ajout de la page home pour un meilleur routage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// src/pages/Home.jsx
+import Header from "../components/Header";
+import Engagement from "./Engagement";
+import Actuality from "./Actuality";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Engagement />
+      <Actuality />
+    </>
+  );
+}

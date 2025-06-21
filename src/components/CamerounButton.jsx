b7fbd8dc63abbf0364fc188d8f5e4ee3efcdd615
Papa G <florentgerard@sfr.fr>
2025-06-21 16:23:07 +0200

    rennomage css et lien

--- src/components/CamerounButton.jsx
+++ src/components/CamerounButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/cameroun-button.css";
+import "../styles/camerounButton.css";
 
 function CamerounButton({ children, to = "#", style = {} }) {
   return (

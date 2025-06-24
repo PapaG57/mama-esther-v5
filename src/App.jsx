925bf7a3fb8126c0ee4be32980df7461696a3c8e
Papa G <florentgerard@sfr.fr>
2025-06-24 18:27:28 +0200

    suppression pour autre création fichier

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,46 @@
-// src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
 import Home from "./pages/Home";
 import AboutPage from "./pages/AboutPage";
 import TeamCarousel from "./pages/TeamCarousel";
 import Actuality from "./pages/Actuality";
-import Footer from "./components/Footer";
 import NewsletterReader from "./pages/newsletters/Reader";
 
-export default function App() {
+import Footer from "./components/Footer";
+
+// Composant englobant pour détecter la route actuelle
+function AppWrapper() {
+  const location = useLocation();
+
+  // On masque le footer sur les pages /newsletter/:id
+  const hideFooter = /^\/newsletter\/\d+$/.test(location.pathname);
+
   return (
-    <Router>
+    <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/equipe" element={<TeamCarousel />} />
         <Route path="/actuality" element={<Actuality />} />
-        {/* ... autres routes */}
         <Route path="/newsletter/:id" element={<NewsletterReader />} />
+        {/* ... autres routes si besoin */}
       </Routes>
-      <Footer />
+      {!hideFooter && <Footer />}
+    </>
+  );
+}
+
+// Router principal
+export default function App() {
+  return (
+    <Router>
+      <AppWrapper />
     </Router>
   );
 }

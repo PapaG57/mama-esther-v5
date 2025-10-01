f3fd180c9324869df949e3831b43f0a983099741
Papa G <florentgerard@sfr.fr>
2025-10-01 19:09:04 +0200

    Suppression du workflow GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mama-esther-v5/", // ← ajoute cette ligne
 });
